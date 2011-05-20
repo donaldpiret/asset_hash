8f6c950708b8af325ab030fc737cb481dd836f69
Aaron Gibralter <aaron.gibralter@gmail.com>
2011-05-20 15:53:58 -0400

    Allow asset_hash to work with Rails 2.3.x.

--- lib/asset_hash/railtie.rb
+++ lib/asset_hash/railtie.rb
@@ -1,9 +1,12 @@
 require 'asset_hash'
-require 'rails'
-module AssetHash
-  class Railtie < Rails::Railtie
-    rake_tasks do
-      load "tasks/asset_hash.rake"
+begin
+  require 'rails'
+  module AssetHash
+    class Railtie < Rails::Railtie
+      rake_tasks do
+        load "tasks/asset_hash.rake"
+      end
     end
   end
+rescue LoadError
 end
