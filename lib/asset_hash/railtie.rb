2e97ed4040249c1a394899fd1395f0f5784b318c
Aaron Gibralter <aaron.gibralter@gmail.com>
2011-05-20 17:15:50 -0400

    Better way of ensuring railtie is only Rails 3.

--- lib/asset_hash/railtie.rb
+++ lib/asset_hash/railtie.rb
@@ -1,12 +1,9 @@
 require 'asset_hash'
-begin
-  require 'rails'
-  module AssetHash
-    class Railtie < Rails::Railtie
-      rake_tasks do
-        load "tasks/asset_hash.rake"
-      end
+require 'rails'
+module AssetHash
+  class Railtie < Rails::Railtie
+    rake_tasks do
+      load "tasks/asset_hash.rake"
     end
   end
-rescue LoadError
 end
